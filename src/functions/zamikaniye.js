c576e1a5c3db0f001da067b64b8952853e9b619f
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-11-23 13:15:12 +0500

    fix project

--- src/functions/zamikaniye.js
+++ src/functions/zamikaniye.js
@@ -1,20 +1,18 @@
-// stack of calls
-
 function f1(name) {
     setTimeout(function (){
         console.log(name + " -> Zarina")
         name = "Zarina"
-    }, 2000)
+    }, 3000)
     return function (age) {
         console.log(name, age)
     }
 }
 
-var f11 = f1("Baki")
+const f11 = f1("Baki");
 
 f11(33)
 
 setTimeout(function (){
     f11(33)
-}, 3000)
+}, 4000)
 
