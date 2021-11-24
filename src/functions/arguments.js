c3aa8187ddd89f656a279190f2d66a0ecf4437aa
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-11-24 18:23:31 +0500

    arguments

--- src/functions/arguments.js
+++ src/functions/arguments.js
@@ -1,9 +1,16 @@
 function test(a, b) {
-    let argLength = arguments.length;
+    let argLength = arguments.length; //[42, 33, 45]
     console.log("Передал " + argLength + " параметров")
+
+    let first = arguments[0];
+    console.log(first)
+
+    let last = arguments[argLength - 1];
+    console.log(last)
 }
 
 test()
 test(42)
 test(42, 33)
 test(42, 33, 45)
+test(42, 33, 45, 23, 234, 35, 566, 575, "23423", 66)
